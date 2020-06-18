0dc96bff88b1513b606a9cc79eac5bb02c9410e9
Ondřej Ezr <oezr@redhat.com>
2020-06-19 00:10:09 +0200

    Move to isolated namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
-Rails.application.routes.draw do
+ForemanPuppetEnc::Engine.routes.draw do
   get 'new_action', to: 'foreman_puppet_enc/hosts#new_action'
   get 'foreman_puppet_enc', to: 'foreman_puppet_enc/react#index'
+
+  namespace :api, defaults: { format: 'json' } do
+    scope '(:apiv)', module: :v2, defaults: { apiv: 'v2' }, apiv: /v1|v2/, constraints: ApiConstraints.new(version: 2, default: true) do
+      get 'new_action', to: 'foreman_puppet_enc/api/v2/hosts#new_action'
+    end
+  end
+end
+
+Foreman::Application.routes.draw do
+  mount ForemanPuppetEnc::Engine, at: '/foreman_puppet'
 end
