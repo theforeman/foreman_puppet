f5b49a59b9436aa5bbd290cb755ab55f59189175
Ondřej Ezr <oezr@redhat.com>
2020-07-20 12:46:28 +0200

    Rubocop perfection for PuppetclassLookupKey

--- app/models/concerns/foreman_puppet_enc/environment_class_decorations.rb
+++ app/models/concerns/foreman_puppet_enc/environment_class_decorations.rb
@@ -3,24 +3,24 @@ module ForemanPuppetEnc
     extend ActiveSupport::Concern
 
     included do
-      belongs_to :puppetclass_lookup_key, :inverse_of => :environment_classes, class_name: 'ForemanPuppetEnc::PuppetclassLookupKey'
+      belongs_to :puppetclass_lookup_key, inverse_of: :environment_classes, class_name: 'ForemanPuppetEnc::PuppetclassLookupKey'
 
-      validates :puppetclass_lookup_key_id, :uniqueness => { :scope => %i[environment_id puppetclass_id] }
+      validates :puppetclass_lookup_key_id, uniqueness: { scope: %i[environment_id puppetclass_id] }
 
       after_destroy :delete_orphaned_lookup_keys
 
       scope :parameters_for_class, lambda { |puppetclasses_ids, environment_id|
-        all_parameters_for_class(puppetclasses_ids, environment_id).where(:puppetclass_lookup_keys => { :override => true })
+        all_parameters_for_class(puppetclasses_ids, environment_id).where(puppetclass_lookup_keys: { override: true })
       }
 
       scope :all_parameters_for_class, lambda { |puppetclasses_ids, environment_id|
-        where(:puppetclass_id => puppetclasses_ids, :environment_id => environment_id)
+        where(puppetclass_id: puppetclasses_ids, environment_id: environment_id)
           .where('puppetclass_lookup_key_id is NOT NULL')
           .includes(:puppetclass_lookup_key)
       }
 
       scope :used_by_other_environment_classes, lambda { |puppetclass_lookup_key_id, this_environment_class_id|
-        where(:puppetclass_lookup_key_id => puppetclass_lookup_key_id)
+        where(puppetclass_lookup_key_id: puppetclass_lookup_key_id)
           .where("id != #{this_environment_class_id}")
       }
     end
@@ -29,12 +29,12 @@ module ForemanPuppetEnc
       # TODO: move these into scopes?
       # rubocop:disable Naming/PredicateName
       def is_in_any_environment(puppetclass, puppetclass_lookup_key)
-        EnvironmentClass.where(:puppetclass_id => puppetclass, :puppetclass_lookup_key_id => puppetclass_lookup_key).count.positive?
+        EnvironmentClass.where(puppetclass_id: puppetclass, puppetclass_lookup_key_id: puppetclass_lookup_key).count.positive?
       end
       # rubocop:enable Naming/PredicateName
 
       def key_in_environment(env, puppetclass, puppetclass_lookup_key)
-        EnvironmentClass.find_by(:environment_id => env, :puppetclass_id => puppetclass, :puppetclass_lookup_key_id => puppetclass_lookup_key)
+        EnvironmentClass.find_by(environment_id: env, puppetclass_id: puppetclass, puppetclass_lookup_key_id: puppetclass_lookup_key)
       end
     end
 
--- app/models/foreman_puppet_enc/puppetclass_lookup_key.rb
+++ app/models/foreman_puppet_enc/puppetclass_lookup_key.rb
@@ -1,16 +1,16 @@
 module ForemanPuppetEnc
   class PuppetclassLookupKey < LookupKey
-    has_many :environment_classes, :dependent => :destroy, :inverse_of => :puppetclass_lookup_key
-    has_many :environments, -> { distinct }, :through => :environment_classes
-    has_many :param_classes, :through => :environment_classes, :source => :puppetclass
+    has_many :environment_classes, dependent: :destroy, inverse_of: :puppetclass_lookup_key
+    has_many :environments, -> { distinct }, through: :environment_classes
+    has_many :param_classes, through: :environment_classes, source: :puppetclass
 
-    before_validation :check_override_selected, :if => -> { persisted? && @validation_context != :importer }
+    before_validation :check_override_selected, if: -> { persisted? && @validation_context != :importer }
 
-    scoped_search :relation => :param_classes, :on => :name, :rename => :puppetclass, :aliases => [:puppetclass_name], :complete_value => true
-    scoped_search :relation => :environments, :on => :name, :rename => :environment, :complete_value => true, :only_explicit => true
+    scoped_search relation: :param_classes, on: :name, rename: :puppetclass, aliases: [:puppetclass_name], complete_value: true
+    scoped_search relation: :environments, on: :name, rename: :environment, complete_value: true, only_explicit: true
 
     scope :smart_class_parameters_for_class, lambda { |puppetclass_ids, environment_id|
-                                               joins(:environment_classes).where(:environment_classes => {:puppetclass_id => puppetclass_ids, :environment_id => environment_id})
+                                               joins(:environment_classes).where(environment_classes: { puppetclass_id: puppetclass_ids, environment_id: environment_id })
                                              }
 
     scope :parameters_for_class, lambda { |puppetclass_ids, environment_id|
@@ -20,7 +20,7 @@ module ForemanPuppetEnc
     scope :smart_class_parameters, -> { joins(:environment_classes).readonly(false) }
 
     def editable_by_user?
-      PuppetclassLookupKey.authorized(:edit_external_parameters).where(:id => id).exists?
+      PuppetclassLookupKey.authorized(:edit_external_parameters).where(id: id).exists?
     end
 
     def param_class
@@ -46,13 +46,13 @@ module ForemanPuppetEnc
     end
 
     def check_override_selected
-      return if (changed - ['description', 'override']).empty?
+      return if (changed - %w[description override]).empty?
       return if override?
-      errors.add(:override, _("must be true to edit the parameter"))
+      errors.add(:override, _('must be true to edit the parameter'))
     end
 
     def self.humanize_class_name
-      "Smart class parameter"
+      'Smart class parameter'
     end
   end
 end
