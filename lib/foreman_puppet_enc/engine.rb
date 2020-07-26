0e584074d607e71cd3b41f4d85d88663eaa0814b
Shira Maximov <shiramaximov@gmail.com>
2020-07-26 14:28:42 +0300

    Add Config Groups UI

--- lib/foreman_puppet_enc/engine.rb
+++ lib/foreman_puppet_enc/engine.rb
@@ -3,6 +3,15 @@ module ForemanPuppetEnc
     engine_name 'foreman_puppet_enc'
     isolate_namespace ForemanPuppetEnc
 
+    config.autoload_paths += Dir["#{config.root}/app/controllers/concerns"]
+    config.autoload_paths += Dir["#{config.root}/app/helpers/concerns"]
+    config.autoload_paths += Dir["#{config.root}/app/models/concerns"]
+    config.autoload_paths += Dir["#{config.root}/app/overrides"]
+
+    if Gem::Dependency.new('', '>= 2.2').match?('', SETTINGS[:version])
+      config.paths['db/migrate'] << 'db/migrate_foreman'
+    end
+
     # Add any db migrations
     initializer 'foreman_puppet_enc.load_app_instance_data' do |app|
       ForemanPuppetEnc::Engine.paths['db/migrate'].existent.each do |path|
@@ -13,17 +22,55 @@ module ForemanPuppetEnc
     initializer 'foreman_puppet_enc.register_plugin', before: :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_puppet_enc do
         requires_foreman '>= 2.0.0'
-
         # Add Global JS file for extending foreman-core components and routes
         register_global_js_file 'fills'
 
         # Add permissions
-        # security_block :foreman_puppet_enc do
-        # end
+        security_block :foreman_puppet_enc do
+          permission :view_config_groups, { :config_groups => %i[index auto_complete_search welcome],
+                                            :"api/v2/config_groups" => %i[index show],
+                                            :'foreman_puppet_enc/react' => [:index] }
+          permission :create_config_groups, { :config_groups => %i[new create],
+                                              :"api/v2/config_groups" => [:create] }
+          permission :edit_config_groups, { :config_groups => %i[edit update],
+                                            :"api/v2/config_groups" => [:update] }
+          permission :destroy_config_groups, { :config_groups => [:destroy],
+                                               :"api/v2/config_groups" => [:destroy] }
+        end
+
+        role 'ForemanPuppetEnc', [:view_foreman_puppet_enc]
+
+        # add puppet ENC divider
+        divider :top_menu, :parent => :configure_menu, :after => :configure, :caption => N_('Puppet ENC')
 
         # add menu entry
-
+        add_menu_item :top_menu, :puppet_enc, {
+          :caption => 'Config Groups',
+          :engine => ForemanPuppetEnc::Engine, :parent => :configure_menu, :after => :ansible,
+          :url_hash => { :controller => 'foreman_puppet_enc/config_groups', :action => :index }
+        }
         # add dashboard widget
+        widget 'foreman_puppet_enc_widget', name: N_('Foreman plugin template widget'), sizex: 4, sizey: 1
+
+        # register host and hostgroup facet
+        # register_facet ForemanPuppetEnc::Host::PuppetFacet, :puppet_facet do
+        #   configure_host
+        #   configure_hostgroup(::ForemanPuppetEnc::Hostgroup::PuppetFacet)
+        # end
+
+        # extend host form with puppet ENC Tab
+        extend_page('hosts/_form') do |context|
+          context.add_pagelet :main_tabs,
+            :name => N_('Puppet ENC'),
+            :partial => 'foreman_puppet_enc/config_groups/config_groups_selection'
+        end
+
+        # extend hostgroup form with puppet ENC Tab
+        extend_page('hostgroups/_form') do |context|
+          context.add_pagelet :main_tabs,
+            :name => N_('Puppet ENC'),
+            :partial => 'foreman_puppet_enc/config_groups/config_groups_selection'
+        end
       end
     end
 
@@ -38,6 +85,7 @@ module ForemanPuppetEnc
       EnvironmentClass.include ForemanPuppetEnc::EnvironmentClassDecorations
       Puppetclass.include ForemanPuppetEnc::PuppetclassDecorations
       HostsHelper.include ForemanPuppetEnc::HostsHelperExtensions
+      HostsController.include ForemanPuppetEnc::Concerns::Puppet::HostsControllerExtensions
     end
 
     rake_tasks do
