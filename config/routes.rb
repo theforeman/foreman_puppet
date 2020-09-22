4475acf8951d9485ff6af861c9d5e4ced54bd16a
Ondřej Ezr <oezr@redhat.com>
2020-09-22 16:43:20 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 ForemanPuppetEnc::Engine.routes.draw do
   get 'new_action', to: 'foreman_puppet_enc/hosts#new_action'
   get 'foreman_puppet_enc', to: 'foreman_puppet_enc/react#index'
+  resources :config_groups, except: :show do
+    collection do
+      get 'help', action: :welcome
+      get 'auto_complete_search'
+    end
+  end
 
   namespace :api, defaults: { format: 'json' } do
     scope '(:apiv)', module: :v2, defaults: { apiv: 'v2' }, apiv: /v1|v2/, constraints: ApiConstraints.new(version: 2, default: true) do
