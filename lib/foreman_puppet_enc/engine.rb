fb4bdb00be3681c0c4620ca7d9543675ae2d6f29
Shira Maximov <shiramaximov@gmail.com>
2020-09-26 15:12:27 +0300

    Add Config Groups UI
    UI for ConfigGroups and a template to add ConfigGroups to a Host.

--- lib/foreman_puppet_enc/engine.rb
+++ lib/foreman_puppet_enc/engine.rb
@@ -8,19 +8,53 @@ module ForemanPuppetEnc
     initializer 'foreman_puppet_enc.register_plugin', before: :finisher_hook do |_app|
       Foreman::Plugin.register :foreman_puppet_enc do
         requires_foreman '>= 2.0.0'
-
         # Add Global JS file for extending foreman-core components and routes
         register_global_js_file 'fills'
 
         # Add permissions
-        # security_block :foreman_puppet_enc do
-        # end
+        security_block :foreman_puppet_enc do
+          permission :view_config_groups, { config_groups: %i[index auto_complete_search welcome],
+                                            "foreman_puppet_enc/api/v2/config_groups": %i[index show],
+                                            'foreman_puppet_enc/react': [:index] }
+          permission :create_config_groups, { config_groups: %i[new create],
+                                              "foreman_puppet_enc/api/v2/config_groups": [:create] }
+          permission :edit_config_groups, { config_groups: %i[edit update],
+                                            "foreman_puppet_enc/api/v2/config_groups": [:update] }
+          permission :destroy_config_groups, { config_groups: [:destroy],
+                                               "foreman_puppet_enc/api/v2/config_groups": [:destroy] }
+        end
+
+        # add puppet ENC divider
+        divider :top_menu, parent: :configure_menu, after: :configure, caption: N_('Puppet ENC')
 
         # add menu entry
-
-        # add dashboard widget
+        add_menu_item :top_menu, :puppet_enc, {
+          caption: 'Config Groups',
+          engine: ForemanPuppetEnc::Engine, parent: :configure_menu, after: :ansible,
+          url_hash: { controller: 'foreman_puppet_enc/config_groups', action: :index }
+        }
 
         register_info_provider(ForemanPuppetEnc::HostInfoProviders::PuppetInfo)
+
+        # register host and hostgroup facet
+        # register_facet ForemanPuppetEnc::Host::PuppetFacet, :puppet_facet do
+        #   configure_host
+        #   configure_hostgroup(::ForemanPuppetEnc::Hostgroup::PuppetFacet)
+        # end
+
+        # extend host form with puppet ENC Tab
+        extend_page('hosts/_form') do |context|
+          context.add_pagelet :main_tabs,
+            name: N_('Puppet ENC'),
+            partial: 'foreman_puppet_enc/config_groups/config_groups_selection'
+        end
+
+        # extend hostgroup form with puppet ENC Tab
+        extend_page('hostgroups/_form') do |context|
+          context.add_pagelet :main_tabs,
+            name: N_('Puppet ENC'),
+            partial: 'foreman_puppet_enc/config_groups/config_groups_selection'
+        end
       end
     end
 
