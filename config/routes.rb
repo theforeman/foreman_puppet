c04620bbe963245b3745d8649411022fe1bffeb8
Ondřej Ezr <oezr@redhat.com>
2020-09-28 03:25:59 +0200

    Remove example API controller

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,6 @@ ForemanPuppetEnc::Engine.routes.draw do
 
   namespace :api, defaults: { format: 'json' } do
     scope '(:apiv)', module: :v2, defaults: { apiv: 'v2' }, apiv: /v1|v2/, constraints: ApiConstraints.new(version: 2, default: true) do
-      get 'new_action', to: 'foreman_puppet_enc/api/v2/hosts#new_action'
     end
   end
 end
