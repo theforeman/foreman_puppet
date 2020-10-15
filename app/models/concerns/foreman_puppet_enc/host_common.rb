115c957bf5386765936d7c61765bf2cf1a348e18
Ondřej Ezr <oezr@redhat.com>
2020-10-16 00:57:29 +0200

    add hostgroup related puppet extensions

--- app/models/concerns/foreman_puppet_enc/host_common.rb
+++ app/models/concerns/foreman_puppet_enc/host_common.rb
@@ -14,69 +14,67 @@ module ForemanPuppetEnc
       alias_method :all_puppetclasses, :classes
     end
 
-    class_methods do
-      def cg_class_ids
-        cg_ids = if is_a?(Hostgroup)
-                   path.each.map(&:config_group_ids).flatten.uniq
-                 else
-                   hostgroup ? hostgroup.path.each.map(&:config_group_ids).flatten.uniq : []
-                 end
-        ConfigGroupClass.where(config_group_id: (config_group_ids + cg_ids)).pluck(:puppetclass_id)
-      end
+    def cg_class_ids
+      cg_ids = if is_a?(Hostgroup)
+                 path.each.map(&:config_group_ids).flatten.uniq
+               else
+                 hostgroup ? hostgroup.path.each.map(&:config_group_ids).flatten.uniq : []
+               end
+      ConfigGroupClass.where(config_group_id: (config_group_ids + cg_ids)).pluck(:puppetclass_id)
+    end
 
-      def hg_class_ids
-        hg_ids = if is_a?(Hostgroup)
-                   path_ids
-                 elsif hostgroup
-                   hostgroup.path_ids
-                 end
-        HostgroupClass.where(hostgroup_id: hg_ids).pluck(:puppetclass_id)
-      end
+    def hg_class_ids
+      hg_ids = if is_a?(Hostgroup)
+                 path_ids
+               elsif hostgroup
+                 hostgroup.path_ids
+               end
+      HostgroupClass.where(hostgroup_id: hg_ids).pluck(:puppetclass_id)
+    end
 
-      def host_class_ids
-        (is_a?(Host::Base) ? host_classes : hostgroup_classes).map(&:puppetclass_id)
-      end
+    def host_class_ids
+      (is_a?(Host::Base) ? host_classes : hostgroup_classes).map(&:puppetclass_id)
+    end
 
-      def all_puppetclass_ids
-        cg_class_ids + hg_class_ids + host_class_ids
-      end
+    def all_puppetclass_ids
+      cg_class_ids + hg_class_ids + host_class_ids
+    end
 
-      def classes(env = environment)
-        conditions = { id: all_puppetclass_ids }
-        if env
-          env.puppetclasses.where(conditions)
-        else
-          Puppetclass.where(conditions)
-        end
+    def classes(env = environment)
+      conditions = { id: all_puppetclass_ids }
+      if env
+        env.puppetclasses.where(conditions)
+      else
+        Puppetclass.where(conditions)
       end
+    end
 
-      def puppetclass_ids
-        classes.reorder('').pluck('puppetclasses.id')
-      end
+    def puppetclass_ids
+      classes.reorder('').pluck('puppetclasses.id')
+    end
 
-      def classes_in_groups
-        conditions = { id: cg_class_ids }
-        if environment
-          environment.puppetclasses.where(conditions) - parent_classes
-        else
-          Puppetclass.where(conditions) - parent_classes
-        end
+    def classes_in_groups
+      conditions = { id: cg_class_ids }
+      if environment
+        environment.puppetclasses.where(conditions) - parent_classes
+      else
+        Puppetclass.where(conditions) - parent_classes
       end
+    end
 
-      # Returns Puppetclasses of a Host or Hostgroup
-      #
-      # It does not include Puppetclasses of it's ConfigGroupClasses
-      #
-      def individual_puppetclasses
-        ids = host_class_ids - cg_class_ids
-        return puppetclasses if ids.blank? && new_record?
-        Puppetclass.includes(:environments).where(id: ids)
-      end
+    # Returns Puppetclasses of a Host or Hostgroup
+    #
+    # It does not include Puppetclasses of it's ConfigGroupClasses
+    #
+    def individual_puppetclasses
+      ids = host_class_ids - cg_class_ids
+      return puppetclasses if ids.blank? && new_record?
+      Puppetclass.includes(:environments).where(id: ids)
+    end
 
-      def available_puppetclasses
-        return Puppetclass.all.authorized(:view_puppetclasses) if environment.blank?
-        environment.puppetclasses - parent_classes
-      end
+    def available_puppetclasses
+      return Puppetclass.all.authorized(:view_puppetclasses) if environment.blank?
+      environment.puppetclasses - parent_classes
     end
   end
 end
