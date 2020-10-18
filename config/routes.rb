e549acf44abf14bab9ab5201c8f795644135a805
Ondřej Ezr <oezr@redhat.com>
2020-10-18 23:47:34 +0200

    Add Environment UI Controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,15 @@ ForemanPuppetEnc::Engine.routes.draw do
     end
   end
 
+  resources :environments, except: [:show] do
+    collection do
+      get 'help', action: :welcome
+      get 'import_environments'
+      post 'obsolete_and_new'
+      get 'auto_complete_search'
+    end
+  end
+
   constraints(id: %r{[^/]+}) do
     resources :puppetclass_lookup_keys, except: %i[show new create] do
       collection do
