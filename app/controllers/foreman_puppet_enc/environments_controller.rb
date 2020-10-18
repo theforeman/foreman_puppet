04550f0e918b948e17bf48916c2423d1ed1b2337
Ondřej Ezr <oezr@redhat.com>
2020-10-18 23:47:34 +0200

    Add Environment UI Controller

---
+++ app/controllers/foreman_puppet_enc/environments_controller.rb
@@ -0,0 +1,50 @@
+module ForemanPuppetEnc
+  class EnvironmentsController < ApplicationController
+    include Foreman::Controller::Environments
+    include Foreman::Controller::AutoCompleteSearch
+    include Foreman::Controller::Parameters::Environment
+
+    before_action :find_resource, only: %i[edit update destroy]
+
+    def index
+      @environments = resource_base_search_and_page
+    end
+
+    def new
+      @environment = Environment.new
+    end
+
+    def create
+      @environment = Environment.new(environment_params)
+      if @environment.save
+        process_success
+      else
+        process_error
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @environment.update(environment_params)
+        process_success
+      else
+        process_error
+      end
+    end
+
+    def destroy
+      if @environment.destroy
+        process_success
+      else
+        process_error
+      end
+    end
+
+    # TEMPORARY until the model PR gets in
+    def model_of_controller
+      @model_of_controller ||= ::Environment
+    end
+  end
+end
---
+++ test/integration/foreman_puppet_enc/environment_js_test.rb
@@ -0,0 +1,25 @@
+require 'test_puppet_enc_helper'
+require 'integration_test_helper'
+
+module ForemanPuppetEnc
+  class EnvironmentJSTest < IntegrationTestWithJavascript
+    test 'index page' do
+      assert_index_page(environments_path, 'Environments', 'Create Puppet Environment')
+    end
+
+    test 'create new page' do
+      assert_new_button(environments_path, 'Create Puppet Environment', new_environment_path)
+      fill_in 'environment_name', with: 'golive'
+      assert_submit_button(environments_path)
+      assert page.has_link? 'golive'
+    end
+
+    test 'edit page' do
+      visit environments_path
+      click_link 'production'
+      fill_in 'environment_name', with: 'production222'
+      assert_submit_button(environments_path)
+      assert page.has_link? 'production222'
+    end
+  end
+end
