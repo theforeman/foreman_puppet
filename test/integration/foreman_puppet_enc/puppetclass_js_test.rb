f5d6d5f897d48a35c2a0a4037663356697a8b71c
Ondřej Ezr <oezr@redhat.com>
2020-10-31 00:47:25 +0100

    Add Puppetclass integration test

---
+++ test/integration/foreman_puppet_enc/puppetclass_js_test.rb
@@ -0,0 +1,37 @@
+require 'test_puppet_enc_helper'
+require 'integration_test_helper'
+
+module ForemanPuppetEnc
+  class PuppetclassJsTest < IntegrationTestWithJavascript
+    include ForemanPuppetEnc::Engine.routes.url_helpers
+    # intermittent failures:
+    #   PuppetclassIntegrationTest.test_0001_edit page
+
+    let(:environment) { FactoryBot.create(:environment) }
+    let(:puppetclass) { FactoryBot.create(:puppetclass, environments: [environment]) }
+
+    test 'edit page' do
+      visit puppetclasses_path
+      click_link 'vim'
+      assert page.has_no_link? 'Common'
+      find(:xpath, "//a[@data-original-title='Select All']").click
+      assert_submit_button(puppetclasses_path)
+      assert page.has_link? 'vim'
+      assert page.has_link? 'Common'
+    end
+
+    test 'verify key label exists in case key is too long' do
+      smart_class_parameter_long = FactoryBot.create(:puppetclass_lookup_key, puppetclass: puppetclass, variable: 'a' * 50)
+      visit edit_puppetclass_path(puppetclass)
+      click_link 'Smart Class Parameter'
+      assert_equal smart_class_parameter_long.key, page.find("#pill_#{smart_class_parameter_long.id}-#{smart_class_parameter_long.key}")['data-original-title']
+    end
+
+    test 'verify key label is empty in case key is short' do
+      smart_class_parameter_short = FactoryBot.create(:puppetclass_lookup_key, puppetclass: puppetclass, variable: 'a' * 40)
+      visit edit_puppetclass_path(puppetclass)
+      click_link 'Smart Class Parameter'
+      assert_empty page.find("#pill_#{smart_class_parameter_short.id}-#{smart_class_parameter_short.key}")['data-original-title']
+    end
+  end
+end
