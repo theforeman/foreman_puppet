f6884578ce3072d49623c47b9d45e68200177548
Ondřej Ezr <oezr@redhat.com>
2020-11-04 00:05:36 +0100

    Add permissions to default roles
    Forgotten in f4608141b7d15a096c914401071390274a54bca7
    Adds permissions that should go in default role.

--- lib/foreman_puppet_enc/register.rb
+++ lib/foreman_puppet_enc/register.rb
@@ -128,6 +128,10 @@ Foreman::Plugin.register :foreman_puppet_enc do
       resource_type: 'Puppetclass'
   end
 
+  add_all_permissions_to_default_roles
+  Foreman::Plugin::RbacSupport::AUTO_EXTENDED_ROLES |= ['Site manager']
+  add_permissions_to_default_roles('Site manager' => %w[view_puppetclasses import_puppetclasses view_environments import_environments])
+
   # add puppet ENC divider
   divider :top_menu, parent: :configure_menu, caption: N_('Puppet ENC')
 
