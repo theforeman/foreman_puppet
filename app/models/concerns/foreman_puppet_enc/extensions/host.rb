8093ddd07d52cf56a80f4772bb96348832a37ca0
Ondřej Ezr <oezr@redhat.com>
2021-01-28 01:51:31 +0100

    Add searches to host(group)

--- app/models/concerns/foreman_puppet_enc/extensions/host.rb
+++ app/models/concerns/foreman_puppet_enc/extensions/host.rb
@@ -4,12 +4,59 @@ module ForemanPuppetEnc
       extend ActiveSupport::Concern
 
       included do
-        include_in_clone puppet: %i[config_groups host_config_groups host_classes]
+        prepend PrependedMethods
 
         env_assoc = reflect_on_association(:environment)
         env_assoc&.instance_variable_set(:@class_name, 'ForemanPuppetEnc::Environment')
 
-        prepend PrependedMethods
+        include_in_clone puppet: %i[config_groups host_config_groups host_classes]
+
+        scoped_search relation: :environment, on: :name, complete_value: true, rename: :environment
+        scoped_search relation: :puppetclasses, on: :name, complete_value: true, rename: :class, only_explicit: true, operators: ['= ', '~ '],
+                      ext_method: :search_by_deprecated_class
+        scoped_search relation: :puppetclasses, on: :name, complete_value: true, rename: :puppetclass, only_explicit: true, operators: ['= ', '~ '],
+                      ext_method: :search_by_puppetclass
+        scoped_search relation: :config_groups, on: :name, complete_value: true, rename: :config_group, only_explicit: true, operators: ['= ', '~ '],
+                      ext_method: :search_by_config_group
+      end
+
+      class_methods do
+        def search_by_deprecated_class(key, operator, value)
+          Foreman::Deprecation.deprecation_warning('3.2', 'search by `class` has been deprecated, please search by `puppetclass` instead')
+          search_by_puppetclass(key, operator, value)
+        end
+
+        def search_by_puppetclass(_key, operator, value)
+          conditions = sanitize_sql_for_conditions(["puppetclasses.name #{operator} ?", value_to_sql(operator, value)])
+          config_group_ids = ForemanPuppetEnc::ConfigGroup.joins(:puppetclasses).where(conditions).pluck(:id)
+          host_ids         = ::Host.authorized(:view_hosts, Host).joins(puppet: :puppetclasses).where(conditions).distinct.pluck(:id)
+          host_ids        += ForemanPuppetEnc::HostConfigGroup
+                             .where(host_type: 'ForemanPuppetEnc::HostPuppetFacet')
+                             .where(config_group_id: config_group_ids)
+                             .pluck(:host_id)
+          hostgroup_ids = ::Hostgroup.unscoped.with_taxonomy_scope.joins(puppet: :puppetclasses).where(conditions).map(&:subtree_ids)
+          if config_group_ids.any?
+            hostgroup_cgs = ForemanPuppetEnc::HostConfigGroup.where(host_type: 'ForemanPuppetEnc::HostgroupPuppetFacet')
+                                                             .where(config_group_id: config_group_ids)
+            hostgroup_ids += ::Hostgroup.unscoped.with_taxonomy_scope.where(hostgroup_cgs.select(:host_id)).map(&:subtree_ids)
+          end
+
+          conds = []
+          conds << "hosts.id IN(#{host_ids.join(',')})" if host_ids.present?
+          conds << "hosts.hostgroup_id IN(#{hostgroup_ids.uniq.join(',')})" if hostgroup_ids.present?
+          { conditions: conds.join(' OR ').presence || 'hosts.id < 0' }
+        end
+
+        def search_by_config_group(_key, operator, value)
+          conditions = sanitize_sql_for_conditions(["config_groups.name #{operator} ?", value_to_sql(operator, value)])
+          host_ids = ::Host::Managed.joins(puppet: :config_groups).where(conditions).pluck(:id).uniq
+          hostgroup_ids = ::Hostgroup.unscoped.with_taxonomy_scope.joins(puppet: :config_groups).where(conditions).distinct.map(&:subtree_ids).flatten.uniq
+
+          conds = []
+          conds << "hosts.id IN(#{host_ids.join(',')})" if host_ids.present?
+          conds << "hosts.hostgroup_id IN(#{hostgroup_ids.join(',')})" if hostgroup_ids.present?
+          { conditions: conds.join(' OR ').presence || 'hosts.id < 0' }
+        end
       end
 
       # rubocop:disable Naming/MethodName
--- app/models/foreman_puppet_enc/hostgroup_puppet_facet.rb
+++ app/models/foreman_puppet_enc/hostgroup_puppet_facet.rb
@@ -70,21 +70,6 @@ module ForemanPuppetEnc
     before_save :remove_duplicated_nested_class
 
     nested_attribute_for :environment_id
-    scoped_search relation: :config_groups, on: :name,
-                  complete_value: true,
-                  rename: :config_group,
-                  only_explicit: true,
-                  operators: ['= ', '~ '],
-                  ext_method: :search_by_config_group
-
-    def self.search_by_config_group(_key, operator, value)
-      conditions = sanitize_sql_for_conditions(["config_groups.name #{operator} ?", value_to_sql(operator, value)])
-      hostgroup_ids = ::Hostgroup.unscoped.with_taxonomy_scope.joins(:config_groups).where(conditions).map(&:subtree_ids).flatten.uniq
-
-      opts = 'hostgroups.id < 0'
-      opts = "hostgroups.id IN(#{hostgroup_ids.join(',')})" if hostgroup_ids.present?
-      { conditions: opts }
-    end
 
     def all_config_groups
       (config_groups + parent_config_groups).uniq
