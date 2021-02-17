c932dde77bf29501572b0e049df458003ebc119e
Ondřej Ezr <oezr@redhat.com>
2021-02-17 22:54:51 +0100

    Fix new rubocop rules

--- test/controllers/foreman_puppet/environments_controller_test.rb
+++ test/controllers/foreman_puppet/environments_controller_test.rb
@@ -215,7 +215,7 @@ module ForemanPuppet
         # and should result in a disk_tree of {"env1" => ["a", "b", "c"],"env2" => ["a", "b", "c"]}
         envs = HashWithIndifferentAccess.new(env1: %w[a b c], env2: %w[a b c])
         pcs = [HashWithIndifferentAccess.new('a' => { 'name' => 'a', 'module' => '', 'params' => {} })]
-        classes = Hash[pcs.map { |k| [k.keys.first, Foreman::ImporterPuppetclass.new(k.values.first)] }]
+        classes = pcs.map { |k| [k.keys.first, Foreman::ImporterPuppetclass.new(k.values.first)] }.to_h
         Environment.expects(:puppetEnvs).returns(envs).at_least(0)
         ProxyAPI::Puppet.any_instance.stubs(:environments).returns(%w[env1 env2])
         ProxyAPI::Puppet.any_instance.stubs(:classes).returns(classes)
