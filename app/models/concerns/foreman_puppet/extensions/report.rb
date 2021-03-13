c622b6952030d638b11299d51c06e51774674315
Ondřej Ezr <oezr@redhat.com>
2021-03-13 16:32:39 +0100

    Add environment search for Report

--- app/models/concerns/foreman_puppet/extensions/report.rb
+++ app/models/concerns/foreman_puppet/extensions/report.rb
@@ -5,6 +5,24 @@ module ForemanPuppet
 
       included do
         has_one :environment, through: :host
+
+        # for cases when class already inherit before we apply the patch
+        self.class.subclasses.each do |subclass|
+          add_environment_search_to(subclass)
+        end
+      end
+
+      class_methods do
+        def inherited(child)
+          add_environment_search_to(child)
+          super
+        end
+
+        def add_environment_search_to(klass)
+          klass.instance_eval do
+            scoped_search relation: :environment, on: :name, complete_value: true, rename: :environment
+          end
+        end
       end
     end
   end
