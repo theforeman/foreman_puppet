52dfb4ae33e1a213388a142caec9126e030c197f
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2021-04-22 13:39:04 +0200

    Fix extracted_from_core? method

--- lib/foreman_puppet.rb
+++ lib/foreman_puppet.rb
@@ -1,9 +1,9 @@
 module ForemanPuppet
-  FOREMAN_EXTRACTION_VERSION = '2.5'.freeze
+  FOREMAN_EXTRACTION_VERSION = '2.6'.freeze
 
   def self.extracted_from_core?
     ENV['PUPPET_EXTRACTED'] == '1' ||
-      Gem::Dependency.new('', ">= #{FOREMAN_EXTRACTION_VERSION}").match?('', SETTINGS[:version])
+      Gem::Dependency.new('', ">= #{FOREMAN_EXTRACTION_VERSION}").match?('', SETTINGS[:version].notag)
   end
 end
 
