bd71c56f5b3a431f2e7b86319190e66d7d0d1cbb
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2021-07-20 16:38:11 +0200

    Correct gem summary and description

--- locale/gemspec.rb
+++ locale/gemspec.rb
@@ -1,2 +1,2 @@
 # Matches foreman_puppet_enc.gemspec
-_('Allow assigning Puppet environmets and classes to the Foreman Hosts.')
+_('Allow assigning Puppet environments and classes to the Foreman Hosts.')
