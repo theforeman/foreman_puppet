69b3e1ffb17f88da2400368c2ede705f0e526a27
Ondřej Ezr <oezr@redhat.com>
2021-08-02 15:10:07 +0200

    Migration of orphaned Puppetclasses
    When the Host or Hostgroup has no Environment associated, the Facet wont
    get created and thus the following migrations for any Puppet info will
    get skipped.
    
    For Hostgroup, we create the Facet as environment doesn't need to be
    set, but for Host that is invalid state and we clean up the other data.
    
    (cherry picked from commit b3444b0dc9169351cc2bcd1348e929538bc8085e)

--- db/migrate/20201125113903_migrate_puppetclasses_to_facets.foreman_puppet.rb
+++ db/migrate/20201125113903_migrate_puppetclasses_to_facets.foreman_puppet.rb
@@ -7,6 +7,10 @@ class MigratePuppetclassesToFacets < ActiveRecord::Migration[6.0]
     self.table_name = 'hostgroup_classes'
   end
 
+  class FakePuppetclass < ApplicationRecord
+    self.table_name = 'puppetclasses'
+  end
+
   def up
     add_reference :host_classes, :host_puppet_facet, foreign_key: true, index: true
     add_reference :hostgroup_classes, :hostgroup_puppet_facet, foreign_key: true, index: true
@@ -14,11 +18,19 @@ class MigratePuppetclassesToFacets < ActiveRecord::Migration[6.0]
     host_ids = FakeHostClass.all.pluck(:host_id).uniq
     host_ids.each do |host_id|
       host_facet = ForemanPuppet::HostPuppetFacet.find_by(host_id: host_id)
-      ForemanPuppet::HostClass.where(host_id: host_id).update_all(host_puppet_facet_id: host_facet.id)
+      host_classes = ForemanPuppet::HostClass.where(host_id: host_id)
+      if host_facet
+        host_classes.update_all(host_puppet_facet_id: host_facet.id)
+      else
+        deleted_classes = FakePuppetclass.where(id: host_classes.pluck(:puppetclass_id)).pluck(:name)
+        say "deleting classes #{deleted_classes.join(', ')} from Host (id=#{host_id}) because it has no environment"
+        host_classes.delete_all
+      end
     end
     hostgroup_ids = FakeHostgroupClass.all.pluck(:hostgroup_id).uniq
     hostgroup_ids.each do |hostgroup_id|
       hostgroup_facet = ForemanPuppet::HostgroupPuppetFacet.find_by(hostgroup_id: hostgroup_id)
+      hostgroup_facet ||= ForemanPuppet::HostgroupPuppetFacet.create(hostgroup: Hostgroup.unscoped.find(hostgroup_id))
       ForemanPuppet::HostgroupClass.where(hostgroup_id: hostgroup_id).update_all(hostgroup_puppet_facet_id: hostgroup_facet.id)
     end
 
