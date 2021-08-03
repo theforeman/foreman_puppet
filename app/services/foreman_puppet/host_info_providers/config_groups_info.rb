ee49ade22460a5498cc410aba04fadc8efdaa685
Ondřej Ezr <oezr@redhat.com>
2021-08-03 14:41:10 +0200

    Fixes #33204 - host info without Puppet
    (cherry picked from commit f9cf45e5b41eaf0393fe58d82cd831ba7ffc316b)

--- app/services/foreman_puppet/host_info_providers/config_groups_info.rb
+++ app/services/foreman_puppet/host_info_providers/config_groups_info.rb
@@ -2,6 +2,8 @@ module ForemanPuppet
   module HostInfoProviders
     class ConfigGroupsInfo < HostInfo::Provider
       def host_info
+        return {} unless host.puppet
+
         config_groups = (host.puppet.config_groups + host.puppet.parent_config_groups).uniq.map(&:name)
         { 'parameters' => { 'foreman_config_groups' => config_groups } }
       end
