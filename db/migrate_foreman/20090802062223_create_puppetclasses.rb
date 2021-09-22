6d5f790f8080b55eea068179950dc7415cf9f3ce
Ondřej Ezr <oezr@redhat.com>
2021-09-22 17:31:34 +0200

    Rubocop on puppetclass migrations

--- db/migrate_foreman/20090802062223_create_puppetclasses.rb
+++ db/migrate_foreman/20090802062223_create_puppetclasses.rb
@@ -1,20 +1,20 @@
 class CreatePuppetclasses < ActiveRecord::Migration[4.2]
   def up
     create_table :puppetclasses do |t|
-      t.string :name, :limit => 255
-      t.string :nameindicator, :limit => 255
+      t.string :name, limit: 255
+      t.string :nameindicator, limit: 255
       t.integer :operatingsystem_id
 
       t.timestamps null: true
     end
-    create_table :hosts_puppetclasses, :id => false do |t|
-      t.references :puppetclass, :null => false
-      t.references :host, :null => false
+    create_table :hosts_puppetclasses, id: false do |t|
+      t.references :puppetclass, null: false
+      t.references :host, null: false
     end
 
-    create_table :operatingsystems_puppetclasses, :id => false do |t|
-      t.references :puppetclass, :null => false
-      t.references :operatingsystem, :null => false
+    create_table :operatingsystems_puppetclasses, id: false do |t|
+      t.references :puppetclass, null: false
+      t.references :operatingsystem, null: false
     end
   end
 
