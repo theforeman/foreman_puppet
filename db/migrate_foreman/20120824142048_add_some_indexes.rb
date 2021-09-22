9d8e80c95529c771f9e66d5f1b42bc81040291d9
Ondřej Ezr <oezr@redhat.com>
2021-09-22 17:31:34 +0200

    Rubocop on puppetclass migrations

--- db/migrate_foreman/20120824142048_add_some_indexes.rb
+++ db/migrate_foreman/20120824142048_add_some_indexes.rb
@@ -7,15 +7,13 @@ class AddSomeIndexes < ActiveRecord::Migration[4.2]
     add_index :hostgroups_puppetclasses, :hostgroup_id
 
     # turn off Foreign Key checks
-    if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
-      execute "SET CONSTRAINTS ALL DEFERRED;"
-    end
+    execute 'SET CONSTRAINTS ALL DEFERRED;' if ActiveRecord::Base.connection.adapter_name == 'PostgreSQL'
 
-    add_foreign_key "host_classes", "hosts", :name => "host_classes_host_id_fk"
-    add_foreign_key "host_classes", "puppetclasses", :name => "host_classes_puppetclass_id_fk"
-    add_foreign_key "hostgroup_classes", "hostgroups", :name => "hostgroup_classes_hostgroup_id_fk"
-    add_foreign_key "hostgroup_classes", "puppetclasses", :name => "hostgroup_classes_puppetclass_id_fk"
-    add_foreign_key "operatingsystems_puppetclasses", "operatingsystems", :name => "operatingsystems_puppetclasses_operatingsystem_id_fk"
-    add_foreign_key "operatingsystems_puppetclasses", "puppetclasses", :name => "operatingsystems_puppetclasses_puppetclass_id_fk"
+    add_foreign_key 'host_classes', 'hosts', name: 'host_classes_host_id_fk'
+    add_foreign_key 'host_classes', 'puppetclasses', name: 'host_classes_puppetclass_id_fk'
+    add_foreign_key 'hostgroup_classes', 'hostgroups', name: 'hostgroup_classes_hostgroup_id_fk'
+    add_foreign_key 'hostgroup_classes', 'puppetclasses', name: 'hostgroup_classes_puppetclass_id_fk'
+    add_foreign_key 'operatingsystems_puppetclasses', 'operatingsystems', name: 'operatingsystems_puppetclasses_operatingsystem_id_fk'
+    add_foreign_key 'operatingsystems_puppetclasses', 'puppetclasses', name: 'operatingsystems_puppetclasses_puppetclass_id_fk'
   end
 end
--- db/migrate_foreman/20180831115634_add_uniqueness_to_puppetclass_name.rb
+++ db/migrate_foreman/20180831115634_add_uniqueness_to_puppetclass_name.rb
@@ -4,17 +4,17 @@ class AddUniquenessToPuppetclassName < ActiveRecord::Migration[5.1]
   end
 
   def up
-    names = FakePuppetclass.group(:name).count.select { |key, value| value > 1 }.keys
+    names = FakePuppetclass.group(:name).count.select { |_key, value| value > 1 }.keys
     unless names.empty?
       names.each do |name|
-        classes = FakePuppetclass.where :name => name
+        classes = FakePuppetclass.where name: name
         say "#{classes.count} Puppet classes with duplicate name detected: #{name}"
       end
-      raise "Please make sure there are no duplicate Puppet classes before continuing."
+      raise 'Please make sure there are no duplicate Puppet classes before continuing.'
     end
 
     remove_index :puppetclasses, :name
-    add_index :puppetclasses, :name, :unique => true
+    add_index :puppetclasses, :name, unique: true
   end
 
   def down
