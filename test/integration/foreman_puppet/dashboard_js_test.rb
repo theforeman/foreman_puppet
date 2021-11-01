33a84bbe0ae9b90c47334649e9e4c8b770e32488
Ondřej Ezr <oezr@redhat.com>
2021-11-01 12:57:27 +0100

    Add dashboard test for outofsync

---
+++ test/integration/foreman_puppet/dashboard_js_test.rb
@@ -0,0 +1,31 @@
+require 'test_puppet_helper'
+require 'integration_test_helper'
+require 'integration/shared/host_finders'
+require 'integration/shared/host_orchestration_stubs'
+
+module ForemanPuppet
+  class DashboardJSTest < IntegrationTestWithJavascript
+    setup do
+      Dashboard::Manager.reset_user_to_default(users(:admin))
+      Setting[:outofsync_interval] = 35
+    end
+
+    context 'with origin' do
+      setup do
+        Setting[:puppet_out_of_sync_disabled] = true
+      end
+
+      context 'out of sync disabled' do
+        test 'has no out of sync link' do
+          visit dashboard_path
+          wait_for_ajax
+          within "li[data-name='Host Configuration Status for Puppet']" do
+            assert page.has_no_link?('Out of sync hosts')
+            assert page.has_no_link?('Good host reports in the last')
+            assert page.has_link?('Good host with reports')
+          end
+        end
+      end
+    end
+  end
+end
