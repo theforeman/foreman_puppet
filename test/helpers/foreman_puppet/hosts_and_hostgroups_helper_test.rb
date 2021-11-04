6b43c66f0e6be3b90cf4098955f4d0ab4849f72c
Ondřej Ezr <oezr@redhat.com>
2021-11-04 18:45:36 +0100

    Puppet Environment inheritable on HG
    This fixes an issue causing the inheritance of Environment for HG not being possible in UI.
    
    Fixes #218

--- test/helpers/foreman_puppet/hosts_and_hostgroups_helper_test.rb
+++ test/helpers/foreman_puppet/hosts_and_hostgroups_helper_test.rb
@@ -9,49 +9,88 @@ module ForemanPuppet
     include HostsAndHostgroupsHelper
 
     describe 'puppet environment field' do
+      let(:fields) { mock('fields_for') }
+      let(:f) { mock('form_for') }
+      let(:object) { mock('object') }
+      let(:puppet_facet) { mock('puppet') }
+
       setup do
-        @host = mock('host')
-        puppet = mock('puppet')
-        @host.stubs(:hostgroup)
-        @host.stubs(:puppet).returns(puppet)
-        @host.stubs(:id).returns(999)
-        @f = mock('f')
-        @f.stubs(:object).returns(@host)
-        @fields = mock('fields')
-        @fields.stubs(:object).returns(puppet)
-        @f.stubs(:fields_for).yields(@fields)
+        f.stubs(:object).returns(object)
+        f.stubs(:fields_for).yields(fields)
+        fields.stubs(:object).returns(puppet_facet)
       end
 
-      test 'it adds new first level attributes' do
-        @fields.expects(:collection_select).with do |*attrs|
-          select_options, html_options = extract_collection_options(attrs)
-          select_options[:test_select_option] == 'test_value1' &&
-            html_options[:test_html_option] == 'test_value2'
+      describe '#host_puppet_environment_field' do
+        let(:object) do
+          host = mock('host')
+          host.stubs(:hostgroup)
+          host.stubs(:puppet).returns(puppet_facet)
+          host.stubs(:id).returns(999)
+          host
         end
 
-        host_puppet_environment_field(@f, { test_select_option: 'test_value1' }, { test_html_option: 'test_value2' })
-      end
+        test 'it adds new first level attributes' do
+          fields.expects(:collection_select).with do |*attrs|
+            select_options, html_options = extract_collection_options(attrs)
+            select_options[:test_select_option] == 'test_value1' &&
+              html_options[:test_html_option] == 'test_value2'
+          end
 
-      test 'it adds new data attributes' do
-        @fields.expects(:collection_select).with do |*attrs|
-          select_options, html_options = extract_collection_options(attrs)
-          select_options[:test_select_option] == 'test_value1' &&
-            html_options[:data][:test] == 'test_value2'
+          host_puppet_environment_field(f, { test_select_option: 'test_value1' }, { test_html_option: 'test_value2' })
         end
 
-        host_puppet_environment_field(@f, { test_select_option: 'test_value1' }, { data: { test: 'test_value2' } })
+        test 'it adds new data attributes' do
+          fields.expects(:collection_select).with do |*attrs|
+            select_options, html_options = extract_collection_options(attrs)
+            select_options[:test_select_option] == 'test_value1' &&
+              html_options[:data][:test] == 'test_value2'
+          end
+
+          host_puppet_environment_field(f, { test_select_option: 'test_value1' }, { data: { test: 'test_value2' } })
+        end
+
+        test 'it overrides existing attributes' do
+          fields.expects(:collection_select).with do |*attrs|
+            html_options = attrs.pop
+            html_options[:data][:test] == 'some_test_value' &&
+              html_options[:data][:url] == '/test/url'
+          end.returns('')
+
+          html = host_puppet_environment_field(f, { disable_button: false }, { data: { url: '/test/url', test: 'some_test_value' } })
+
+          assert_no_match(/btn/, html)
+        end
       end
 
-      test 'it overrides existing attributes' do
-        @fields.expects(:collection_select).with do |*attrs|
-          html_options = attrs.pop
-          html_options[:data][:test] == 'some_test_value' &&
-            html_options[:data][:url] == '/test/url'
-        end.returns('')
+      describe '#hostgroup_puppet_environment_field' do
+        let(:object) { FactoryBot.build_stubbed(:hostgroup, parent: parent_hg) }
+        let(:puppet_facet) { object.puppet || object.build_puppet }
 
-        html = host_puppet_environment_field(@f, { disable_button: false }, { data: { url: '/test/url', test: 'some_test_value' } })
+        context 'parent without puppet' do
+          let(:parent_hg) { FactoryBot.create(:hostgroup) }
 
-        assert_no_match(/btn/, html)
+          it 'shows Inherit option with no value' do
+            fields.expects(:collection_select).with do |*attrs|
+              options = attrs.second
+              options.first.to_label == 'Inherit parent (no value)'
+            end.returns('')
+
+            hostgroup_puppet_environment_field(f)
+          end
+        end
+
+        context 'parent with puppet' do
+          let(:parent_hg) { FactoryBot.create(:hostgroup, :with_puppet_enc) }
+
+          it 'shows Inherit option with no value' do
+            fields.expects(:collection_select).with do |*attrs|
+              options = attrs.second
+              options.first.to_label == "Inherit parent (#{parent_hg.puppet.environment.name})"
+            end.returns('')
+
+            hostgroup_puppet_environment_field(f)
+          end
+        end
       end
     end
 
