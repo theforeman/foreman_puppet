4c0712de5b712df6e4846fbfad2d368ae9a29c89
Ondřej Ezr <oezr@redhat.com>
2021-12-15 14:57:06 +0100

    Drop foreign_keys only if exist
    Some foreign keys have been already created with other names or not created at all.
    This adds a security measure to remove the key whatever the name is if it exists.
    
    Fixes #239

--- db/migrate_foreman/20120824142048_add_some_indexes.foreman_puppet.rb
+++ db/migrate_foreman/20120824142048_add_some_indexes.foreman_puppet.rb
@@ -13,9 +13,9 @@ class AddSomeIndexes < ActiveRecord::Migration[4.2]
   end
 
   def down
-    remove_foreign_key 'host_classes', 'hosts', name: 'host_classes_host_id_fk'
-    remove_foreign_key 'host_classes', 'puppetclasses', name: 'host_classes_puppetclass_id_fk'
-    remove_foreign_key 'operatingsystems_puppetclasses', 'operatingsystems', name: 'operatingsystems_puppetclasses_operatingsystem_id_fk'
-    remove_foreign_key 'operatingsystems_puppetclasses', 'puppetclasses', name: 'operatingsystems_puppetclasses_puppetclass_id_fk'
+    remove_foreign_key('host_classes', 'hosts') if foreign_key_exists?(:host_classes, :hosts)
+    remove_foreign_key('host_classes', 'puppetclasses') if foreign_key_exists?(:host_classes, :puppetclasses)
+    remove_foreign_key('operatingsystems_puppetclasses', 'operatingsystems') if foreign_key_exists?(:operatingsystems_puppetclasses, :operatingsystems)
+    remove_foreign_key('operatingsystems_puppetclasses', 'puppetclasses') if foreign_key_exists?(:operatingsystems_puppetclasses, :puppetclasses)
   end
 end
