8aad6de1bdd6dbb6f90c4aa987ea22fc8e841122
Amir Fefer <amirfefer@gmail.com>
2022-03-10 15:52:06 +0200

    Add details card

--- webpack/src/Extends/Host/PuppetTab/SubTabs/Reports/index.js
+++ webpack/src/Extends/Host/PuppetTab/SubTabs/Reports/index.js
@@ -5,8 +5,17 @@ import Slot from 'foremanReact/components/common/Slot';
 import CardTemplate from 'foremanReact/components/HostDetails/Templates/CardItem/CardTemplate';
 import { translate as __ } from 'foremanReact/common/I18n';
 import './styles.scss';
+import DescriptionCard from './components/DescriptionCard';
 
-const Reports = ({ hostName }) => (
+const Reports = ({
+  hostName,
+  status,
+  hostInfo: {
+    puppet_proxy_name: proxyName,
+    puppet_ca_proxy_name: caProxy,
+    environment_name: env,
+  },
+}) => (
   <div className="report-tab">
     <Grid hasGutter>
       <GridItem span={8}>
@@ -15,9 +24,12 @@ const Reports = ({ hostName }) => (
         </CardTemplate>
       </GridItem>
       <GridItem span={4}>
-        <CardTemplate header={__('Puppet details')} expandable>
-          TBD
-        </CardTemplate>
+        <DescriptionCard
+          proxyName={proxyName}
+          caProxy={caProxy}
+          env={env}
+          status={status}
+        />
       </GridItem>
       <GridItem span={12}>
         <Slot hostName={hostName} id="[puppet]-reports" />
@@ -28,6 +40,13 @@ const Reports = ({ hostName }) => (
 
 Reports.propTypes = {
   hostName: PropTypes.string.isRequired,
+  hostInfo: PropTypes.object,
+  status: PropTypes.string,
+};
+
+Reports.defaultProps = {
+  hostInfo: {},
+  status: undefined,
 };
 
 export default Reports;
