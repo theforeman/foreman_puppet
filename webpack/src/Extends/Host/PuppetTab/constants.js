023819426caf2ee89ac7522cf7196b7ee33f88c7
Ron Lavi <rlavi@redhat.com>
2022-11-22 11:48:42 +0200

    Remove the tabs that were not yet implemented (#316)
    * Remove the tabs that were not yet implemented
    
    hiding the assigned and smart-classes sub tabs,
    since they were not yet implemented
    
    * Bump foreman_puppet to 5.0.0
    
    0335faa is using a new functionality which requires a new foreman version,
    this should bump our plugin's major version to 5.0.0
    
    * Fix lint issues

--- webpack/src/Extends/Host/PuppetTab/constants.js
+++ webpack/src/Extends/Host/PuppetTab/constants.js
@@ -2,7 +2,9 @@ import { translate as __ } from 'foremanReact/common/I18n';
 
 export const SECONDARY_TABS = [
   { key: 'reports', title: __('Reports') },
-  { key: 'assigned', title: __('Assigned classes') },
-  { key: 'smart-classes', title: __('Smart class parameters') },
+  /** Hiding the two tabs since they were not implemented yet and shouldn't be part of the release.
+   * { key: 'assigned', title: __('Assigned classes') },
+   * { key: 'smart-classes', title: __('Smart class parameters') },
+   */
   { key: 'yaml', title: __('ENC Preview') },
 ];
