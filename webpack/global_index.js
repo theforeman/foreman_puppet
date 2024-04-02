b706bfd16d3a6392d8a8fd8a20b42723c94ee84b
Jeremy Lenz <jlenz@redhat.com>
2024-04-02 16:56:22 -0400

    Add customizable column to React HostsIndex page

--- webpack/global_index.js
+++ webpack/global_index.js
@@ -1,4 +1,6 @@
 import { registerReducer } from 'foremanReact/common/MountingService';
+import { registerColumns } from 'foremanReact/components/HostsIndex/Columns/core';
+import { translate as __ } from 'foremanReact/common/I18n';
 import reducers from './src/reducers';
 import { registerFills } from './src/Extends/Fills';
 import { registerLegacy } from './legacy';
@@ -9,3 +11,22 @@ registerReducer('puppet', reducers);
 registerFills();
 // TODO: the checkForUnavailablePuppetclasses is very nasty
 registerLegacy();
+
+// register columns for React hosts index page
+const puppetHostsIndexColumns = [
+  {
+    columnName: 'environment',
+    title: __('Puppet env'),
+    isSorted: true,
+    wrapper: hostDetails => hostDetails.environment_name,
+    weight: 2700,
+  },
+];
+
+puppetHostsIndexColumns.forEach(column => {
+  column.tableName = 'hosts';
+  column.categoryName = 'Puppet';
+  column.categoryKey = 'puppet';
+});
+
+registerColumns(puppetHostsIndexColumns);
