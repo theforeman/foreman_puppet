de03f41439f2bb4b140f4f7399e42845626cfdd8
Adam Ruzicka <aruzicka@redhat.com>
2026-03-06 15:06:33 +0100

    Fixes #39210 - Fix the tests

--- app/models/concerns/foreman_puppet/extensions/user.rb
+++ app/models/concerns/foreman_puppet/extensions/user.rb
@@ -9,19 +9,7 @@ module ForemanPuppet
 
       module PatchedMethods
         def visible_environments
-          authorized_scope = ForemanPuppet::Environment.unscoped.authorized(:view_environments)
-          authorized_scope = authorized_scope
-                             .joins(:taxable_taxonomies)
-                             .where('taxable_taxonomies.taxonomy_id' => taxonomy_ids[:organizations] + taxonomy_ids[:locations])
-          result = authorized_scope.distinct.pluck(:name)
-          if ::User.current.admin?
-            # Admin users can also see Environments that do not have any organization or location, even when
-            # organizations and locations are enabled.
-            untaxed_env_ids = TaxableTaxonomy.where(taxable_type: 'ForemanPuppet::Environment').distinct.select(:taxable_id)
-            untaxed_environments = ForemanPuppet::Environment.unscoped.where.not(id: untaxed_env_ids).pluck(:name)
-            result += untaxed_environments
-          end
-          result
+          ForemanPuppet::Environment.authorized(:view_environments).pluck(:name)
         end
       end
     end
