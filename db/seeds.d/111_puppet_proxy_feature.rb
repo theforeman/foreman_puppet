c95bf01e1c92ef4a3755591cfba49bfacf3018fc
Lukas Zapletal <lzap+git@redhat.com>
2026-08-20 17:49:01 +0200

    Fixes #39672 - Use SeedHelper.format_errors in seeds

--- db/seeds.d/111_puppet_proxy_feature.rb
+++ db/seeds.d/111_puppet_proxy_feature.rb
@@ -4,5 +4,5 @@ proxy_features = %w[Puppet]
 
 proxy_features.each do |f_name|
   f = Feature.where(name: f_name).first_or_create
-  raise "Unable to create proxy feature: #{format_errors f}" if f.nil? || f.errors.any?
+  raise "Unable to create proxy feature: #{SeedHelper.format_errors(f)}" if f.nil? || f.errors.any?
 end
