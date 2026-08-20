4f445dd66153684bfce2fe9d21732a02add08af1
MariaAga <mariaaga@redhat.com>
2026-08-20 11:32:15 +0100

    Add ouiaId to MenuToggle

--- webpack/src/Extends/Hosts/BulkActions/BulkChangeProxyCommon/index.js
+++ webpack/src/Extends/Hosts/BulkActions/BulkChangeProxyCommon/index.js
@@ -77,6 +77,7 @@ const BulkChangeProxyCommon = ({
 
   const toggle = toggleRef => (
     <MenuToggle
+      ouiaId="bulk-change-proxy-common-select-toggle"
       ref={toggleRef}
       onClick={onToggleClick}
       isExpanded={smartProxySelectOpen}
